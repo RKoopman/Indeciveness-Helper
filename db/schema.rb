278bfaa25843262784692787ef665c2d4ad3e875
RKoopman <raoul.koopman@gmail.com>
2017-03-13 12:11:59 +0100

    edit User#show to Question#show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170310221918) do
+ActiveRecord::Schema.define(version: 20170313105807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,8 +41,9 @@ ActiveRecord::Schema.define(version: 20170310221918) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "question_id"
   end
 
 end
--- notes.md
+++ notes.md
@@ -1,13 +1,8 @@
 - sessions & password
 
 - backbone.js to:
-      - Pros/Cons table:
-        - add, edit, delete pro/con
-        -
-
-      - go through and set it up for just pros.
-      - then go back and set it up for cons. (going to be the same functions, just new variables)
-      - edit User#show to have two tables (Pros & Cons)
+      + Pros/Cons table:
+        + add, edit, delete pro/con
 
 - Pages needed:
   - Log-in
@@ -15,3 +10,7 @@
   - Edit User (form)
   - Show User (list all questions)
   - Show Question (add/edit/delete pros and cons)
+
+- Page alts:
+  - Question#index is on User#show (show all Q's on User page)
+  - Edit/Delete Q's and add pros/cons from Question#show
